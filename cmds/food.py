1dcf54ddaddce5386f5d0d147b2c8064eb9654ff
鐵人 <terry150490@gmail.com>
2021-11-12 02:41:43 +0800

    Update food.py
    by smart_ramet

--- cmds/food.py
+++ cmds/food.py
@@ -1,9 +1,13 @@
+from logging import DEBUG, debug
 import discord
+from discord import message
 from discord.ext import commands
 from core.classes import Cog_Extension
+import re
+
 import json
 import random
-client = discord.Client()
+#client = discord.Client()
 auther_text = ""
 
 
@@ -15,31 +19,28 @@ class Food(Cog_Extension):
 
     @commands.command()
     async def food(self,ctx):
-        username = ctx.message.author.display_name
-        print(username)
+        self.username = ctx.message.author.display_name
+        print(self.username)
         msg = await ctx.send("今天想吃什麼")
-        emoji1 = '\N{LARGE GREEN CIRCLE}'
-        emoji2 = '\N{LARGE YELLOW CIRCLE}'
-        emoji3 = '\N{LARGE RED CIRCLE}'
-        emoji4 = '\N{LARGE BLUE CIRCLE}'
-        await msg.add_reaction(emoji1)
-        await msg.add_reaction(emoji2)
-        await msg.add_reaction(emoji3)
-        await msg.add_reaction(emoji4)
+        self.emojis = ['\N{LARGE GREEN CIRCLE}','\N{LARGE YELLOW CIRCLE}','\N{LARGE RED CIRCLE}','\N{LARGE BLUE CIRCLE}']       
+        await msg.add_reaction(self.emojis[0])
+        await msg.add_reaction(self.emojis[1])
+        await msg.add_reaction(self.emojis[2])
+        await msg.add_reaction(self.emojis[3])
+        
+        
         # emoji = '\N{LARGE GREEN CIRCLE}'
     
     @commands.Cog.listener()
 
-    async def on_message(self,msg):
-        if msg.content == '~food':
-            print(msg.id)
+    # async def on_message(self,msg):
 
 
-    async def on_reaction_add(self,reaction,user):
-        print(reaction)
-        print(user)
-        if str(reaction) == '\N{LARGE GREEN CIRCLE}':
-            if user == '':
+    async def on_reaction_add(self,reaction,user):              
+        #🟢
+        
+        if str(reaction) in self.emojis: 
+            if(self.username==str(user)[:-5]):     
                 print("A")
 
 
